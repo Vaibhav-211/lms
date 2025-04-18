067e80949a1ea018c01fe0ca33eda02ff35fb5d1
Vaibhav-211 <sajalbunkar@gmail.com>
2025-04-18 08:41:45 +0530

    second commit

---
+++ server/models/Course.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const lectureSchema = new mongoose.Schema({
+    lectureId : {type:String,required:true},
+    lectureTitle : {type:String,required:true},
+    lectureDuration : {type:Number,required:true},
+    lectureUrl : {type:String,required:true},
+    isPreviewFree : {type:Boolean,required:true},
+    lectureOrder: {type:Number, required: true}
+}, {_id:false})
+
+const chapterSchema = new mongoose.Schema({
+    chapterId: {type:String, requird:true},
+    chapterOrder: {type:Number, requird:true},
+    chapterTitle: {type:String, requird:true},
+    chapterContent: {lectureSchema},
+}, {_id:false})
+
+const cousreSchema = new mongoose.Schema({
+    courseTitle : {type:String, required: true},
+    courseDescription : {type:String, required: true},
+    courseThumbnail : {type:String},
+    coursePrice : {type:Number, required: true},
+    isPublished : {type:Boolean, required: true},
+    discount : {type:Number, required: true, min: 0, max: 100},
+    courseContent : [chapterSchema],
+    couresRating:[
+        {userId: {type:String} ,rating:{type:Number,min:1,max:5}}
+    ],
+    educator: {type:String, ref:'User', required: true},
+    enrolledStudents:[
+        {type:String, ref: 'User'}
+    ],
+},{timestamps: true, minimize:false})
+
+const Course = mongoose.model('Course', cousreSchema)
+
+export default Course;
--- server/server.js
+++ server/server.js
@@ -3,19 +3,25 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
 
 //initalize express
 const app = express()
 
 //connect to db
 await connectDB()
-
+await connectCloudinary()
 //middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 //Routes
 app.get('/',(req,res)=> res.send("API working"))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
+
 
 //port
 const PORT = process.env.PORT || 5000
