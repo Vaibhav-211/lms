6e2a90ae8707925343b1a9309ef239a21f5220df
Vaibhav-211 <sajalbunkar@gmail.com>
2025-04-18 06:49:03 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+
+//initalize express
+const app = express()
+
+//connect to db
+await connectDB()
+
+//middlewares
+app.use(cors())
+
+//Routes
+app.get('/',(req,res)=> res.send("API working"))
+app.post('/clerk', express.json(), clerkWebhooks)
+
+//port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, (req,res)=>{
+    console.log(`Server is running on ${PORT}`);
+    
+})
